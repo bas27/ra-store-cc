6c81de83d807dab6731be1aaddce5512ab6031e3
Alexander B <bas2784@gmail.com>
2023-08-03 08:18:00 +0300

    add props

--- src/App.js
+++ src/App.js
@@ -2,7 +2,17 @@ import ShopItem from "./components/shop_item/ShopItem";
 
 
 
-export const App = (item) => {
+export const App = () => {
+
+  const item = {
+    brand: 'Tiger of Sweden',
+    title: 'Leonard coat',
+    description: 'Minimalistic coat in cotton-blend',
+    descriptionFull: 'Men\'s minimalistic overcoat in cotton-blend. Features a stand-up collar, concealed front closure and single back vent. Slim fit with clean, straight shape. Above-knee length.',
+    price: 399,
+    currency: '£'
+  }
+
   return (
     <div className="container">
       <div className="background-element">
--- src/components/shop_item/ShopItem.jsx
+++ src/components/shop_item/ShopItem.jsx
@@ -1,17 +1,9 @@
 import { Component } from 'react'
 import './css/main.css'
 
-const item = {
-  brand: 'Tiger of Sweden',
-  title: 'Leonard coat',
-  description: 'Minimalistic coat in cotton-blend',
-  descriptionFull: 'Men\'s minimalistic overcoat in cotton-blend. Features a stand-up collar, concealed front closure and single back vent. Slim fit with clean, straight shape. Above-knee length.',
-  price: 399,
-  currency: '£'
-}
-
 export default class ShopItem extends Component {
   render() {
+    const { item } = this.props;
     return (
       <div class="main-content">
         <h2>{item.brand}</h2>
